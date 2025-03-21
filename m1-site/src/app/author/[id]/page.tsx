c57b68d95bc7045ff732315963a12330947f52e3
DokZokiu <dokzokiu@gmail.com>
2025-03-21 10:30:09 +0100

    AVG Rating book

--- m1-site/src/app/author/[id]/page.tsx
+++ m1-site/src/app/author/[id]/page.tsx
@@ -1,9 +1,11 @@
+'use client'
+import { AuthorDetails } from "../../../components/AuthorDetails";
 import GlobalLayout from "../../../components/GlobalLayout";
 
 export default function BooksPageId() {
     return (
       <GlobalLayout>
-        <h1>Books Page Id</h1>
+        <AuthorDetails />
       </GlobalLayout>
     );
   }
